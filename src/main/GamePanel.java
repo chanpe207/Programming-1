d37a5c4c5225106ea2a6de02d222e38b0734ac67
chanpe207 <chanpe207@gmail.com>
2023-08-20 15:52:02 +1000

    Added Main GamePanel class and window intialisation methods

---
+++ src/main/GamePanel.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel {
+    //Screen Settings
+    final int originalTileSize = 16; //16 x 16 default tile size (players, mobs, etc)
+    final int scale = 3; //rescales tiles so 16 x 16 appears 48 x 48
+
+    final int tileSize = originalTileSize * scale;
+    final int maxScreenCol = 18;
+    final int maxScreenRow = 16;
+    final int screenWidth = tileSize * maxScreenCol; // 864 wide
+    final int screenHeight = tileSize * maxScreenRow; // 768 high
+
+    public GamePanel() {
+
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+    }
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,19 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
 
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Charlize's Game");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
     }
 }
