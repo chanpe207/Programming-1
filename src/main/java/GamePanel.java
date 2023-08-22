d68cd8fb9a85a3f2d35b09170ee80b402c3af299
chanpe207 <chanpe207@gmail.com>
2023-08-22 15:51:08 +1000

    Added KeyHandler Class, linked keypresses to graphics updates, created FPS timer in gameloop

--- src/main/java/GamePanel.java
+++ src/main/java/GamePanel.java
@@ -12,13 +12,24 @@ public class GamePanel extends JPanel implements Runnable{
     final int screenWidth = tileSize * maxScreenCol; // 864 wide
     final int screenHeight = tileSize * maxScreenRow; // 672 high
 
+    // FPS
+    int FPS = 60;
+
+    KeyHandler keyH = new KeyHandler(); //instantiates keyHandler class
     Thread gameThread; //allows game to run over time
+
+    //Set player's default position
+    int playerX = 200;
+    int playerY = 200;
+    int playerSpeed = 3;
 
     public GamePanel() {
 
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
     }
 
     public void startGameThread() {
@@ -30,18 +41,59 @@ public class GamePanel extends JPanel implements Runnable{
     @Override
     public void run() {
         // GAME LOOP
+
+        // TIME: set the length of time for each iteration
+        double drawInterval = 1000000000/FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+        int drawCount = 0;
+
         while(gameThread != null) {
 
-            // UPDATE: updates information such as character positions
-            update();
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) / drawInterval; // delta is a ratio of time passed to next draw interval
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
 
-            // DRAW: draw the screen with the updated information
-            repaint();
+            if(delta > 1) {
+                // UPDATE: updates information such as character positions
+                update();
+
+                // DRAW: draw the screen with the updated information
+                repaint(); //calls paintComponent
+
+                delta--; //reset delta
+                drawCount++;
+            }
+
+            if(timer >= 1000000000) {
+                System.out.println("FPS:" + drawCount);
+                drawCount = 0;
+                timer = 0;
+            }
+
+
         }
 
     }
 
     public void update() {
+
+        //Key presses
+        if(keyH.upPressed == true) {
+            playerY -= playerSpeed;
+        }
+        else if(keyH.downPressed == true) {
+            playerY += playerSpeed;
+        }
+        else if(keyH.rightPressed == true) {
+            playerX += playerSpeed;
+        }
+        else if(keyH.leftPressed == true) {
+            playerX -= playerSpeed;
+        }
 
     }
 
@@ -53,7 +105,7 @@ public class GamePanel extends JPanel implements Runnable{
 
         g2.setColor(Color.white);
 
-        g2.fillRect(100, 100, tileSize, tileSize);
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
 
         g2.dispose(); //disposes of graphics context and releases system resources to save memory
 
