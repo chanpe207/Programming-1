0c03e066cb5c8adb4384155af26d071cf2ca697b
chanpe207 <chanpe207@gmail.com>
2023-09-18 15:14:22 +1000

    Created Added doors, keys and chests to map

--- src/main/java/AssetSetter.java
+++ src/main/java/AssetSetter.java
@@ -9,12 +9,24 @@ public class AssetSetter {
     public void setObject() {
 
         gp.obj[0] = new OBJ_Key();
-        gp.obj[0].worldX = 14 * gp.tileSize;
-        gp.obj[0].worldY = 14 * gp.tileSize;
+        gp.obj[0].worldX = 6 * gp.tileSize;
+        gp.obj[0].worldY = 27 * gp.tileSize;
 
         gp.obj[1] = new OBJ_Key();
         gp.obj[1].worldX = 20 * gp.tileSize;
         gp.obj[1].worldY = 20 * gp.tileSize;
+
+        gp.obj[2] = new OBJ_Door();
+        gp.obj[2].worldX = 3 * gp.tileSize;
+        gp.obj[2].worldY = 3 * gp.tileSize;
+
+        gp.obj[3] = new OBJ_Door();
+        gp.obj[3].worldX = 44 * gp.tileSize;
+        gp.obj[3].worldY = 19 * gp.tileSize;
+
+        gp.obj[4] = new OBJ_Chest();
+        gp.obj[4].worldX = 37 * gp.tileSize;
+        gp.obj[4].worldY = 30 * gp.tileSize;
 
     }
 }
