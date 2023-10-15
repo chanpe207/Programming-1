20f12779ed00734c0560423a65cff7d8c9efe5de
chanpe207 <chanpe207@gmail.com>
2023-10-15 11:42:51 +1100

    Created EventHandler

--- src/main/java/GamePanel.java
+++ src/main/java/GamePanel.java
@@ -30,6 +30,7 @@ public class GamePanel extends JPanel implements Runnable{
     CollisionChecker cChecker = new CollisionChecker(this);
     public AssetSetter aSetter = new AssetSetter(this);
     public UI ui = new UI(this);
+    public EventHandler eHandler = new EventHandler(this);
     Thread gameThread; //allows game to run over time
 
     // Entities and Objects
